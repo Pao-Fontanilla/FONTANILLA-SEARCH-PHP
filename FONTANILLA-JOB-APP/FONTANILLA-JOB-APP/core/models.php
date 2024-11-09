df8177825634518d5d57b395c0593e25d9b597cd
Pao-Fontanilla <158665654+Pao-Fontanilla@users.noreply.github.com>
2024-11-09 20:46:32 +0800

    Add files via upload

---
+++ FONTANILLA-JOB-APP/FONTANILLA-JOB-APP/core/models.php
@@ -0,0 +1,105 @@
+<?php  
+
+require_once 'dbConfig.php';
+
+function getAllUsers($pdo) {
+	$sql = "SELECT * FROM search_users_data 
+			ORDER BY date_added DESC";
+	$stmt = $pdo->prepare($sql);
+	$executeQuery = $stmt->execute();
+	if ($executeQuery) {
+		return $stmt->fetchAll();
+	}
+}
+
+function getUserByID($pdo, $id) {
+	$sql = "SELECT * from search_users_data WHERE id = ?";
+	$stmt = $pdo->prepare($sql);
+	$executeQuery = $stmt->execute([$id]);
+
+	if ($executeQuery) {
+		return $stmt->fetch();
+	}
+}
+
+function searchForAUser($pdo, $searchQuery) {
+	
+	$sql = "SELECT * FROM search_users_data WHERE 
+			CONCAT(first_name,last_name,email,gender,
+				address,state,nationality,ac_specialty,date_added) 
+			LIKE ?";
+
+	$stmt = $pdo->prepare($sql);
+	$executeQuery = $stmt->execute(["%".$searchQuery."%"]);
+	if ($executeQuery) {
+		return $stmt->fetchAll();
+	}
+}
+
+function insertNewUser($pdo, $first_name, $last_name, $email, 
+	$gender, $address, $state, $nationality, $ac_specialty) {
+
+	$sql = "INSERT INTO search_users_data 
+			(
+				first_name,
+				last_name,
+				email,
+				gender,
+				address,
+				state,
+				nationality,
+				ac_specialty
+			)
+			VALUES (?,?,?,?,?,?,?,?)
+			";
+
+	$stmt = $pdo->prepare($sql);
+	$executeQuery = $stmt->execute([
+		$first_name, $last_name, $email, 
+		$gender, $address, $state, 
+		$nationality, $ac_specialty,
+	]);
+
+	if ($executeQuery) {
+		return true;
+	}
+
+}
+
+function editUser($pdo, $first_name, $last_name, $email, $gender, 
+	$address, $state, $nationality, $ac_specialty, $id) {
+
+	$sql = "UPDATE search_users_data
+				SET first_name = ?,
+					last_name = ?,
+					email = ?,
+					gender = ?,
+					address = ?,
+					state = ?,
+					nationality = ?,
+					ac_specialty = ?
+				WHERE id = ? 
+			";
+
+	$stmt = $pdo->prepare($sql);
+	$executeQuery = $stmt->execute([$first_name, $last_name, $email, $gender, 
+		$address, $state, $nationality,$ac_specialty, $id]);
+
+	if ($executeQuery) {
+		return true;
+	}
+
+}
+
+function deleteUser($pdo, $id) {
+	$sql = "DELETE FROM search_users_data 
+			WHERE id = ?";
+	$stmt = $pdo->prepare($sql);
+	$executeQuery = $stmt->execute([$id]);
+
+	if ($executeQuery) {
+		return true;
+	}
+}
+
+?>
---
+++ FONTANILLA-JOB-APP/FONTANILLA-JOB-APP/sql/data.sql
@@ -0,0 +1,33 @@
+create table search_users_data (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	first_name VARCHAR(255),
+	last_name VARCHAR(255),
+	email VARCHAR(255),
+	gender VARCHAR(255),
+	address VARCHAR(255),
+	state VARCHAR(255),
+	nationality VARCHAR(255),
+	ac_specialty VARCHAR(255),
+	date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+insert into search_users_data (id, first_name, last_name, email, gender, address, state, nationality, ac_specialty, date_added) values (1, 'Ramsey', 'Lambourne', 'rlambourne0@amazon.com', 'Male', 'Suite 51', 'Arizona', 'Sweden', 'Window-Type', '2024-10-13 21:18:31');
+insert into search_users_data (id, first_name, last_name, email, gender, address, state, nationality, ac_specialty, date_added) values (2, 'Isahella', 'Mordan', 'imordan1@t-online.de', 'Female', 'Room 249', 'Arizona', 'Philippines', 'Portable-Type', '2024-05-01 00:25:26');
+insert into search_users_data (id, first_name, last_name, email, gender, address, state, nationality, ac_specialty, date_added) values (3, 'Earlie', 'Berens', 'eberens2@reference.com', 'Male', 'Suite 20', 'Washington', 'China', 'Central Air-Type', '2024-02-15 15:35:49');
+insert into search_users_data (id, first_name, last_name, email, gender, address, state, nationality, ac_specialty, date_added) values (4, 'Torrance', 'Doman', 'tdoman3@miibeian.gov.cn', 'Male', 'PO Box 89839', 'Davao', 'Colombia', 'Window-Type', '2024-02-04 03:30:53');
+insert into search_users_data (id, first_name, last_name, email, gender, address, state, nationality, ac_specialty, date_added) values (5, 'Percy', 'Deverock', 'pdeverock4@parallels.com', 'Male', '17th Floor', 'Cagayan de Oro.', 'Bolivia', 'Window-Type', '2024-02-01 00:14:30');
+insert into search_users_data (id, first_name, last_name, email, gender, address, state, nationality, ac_specialty, date_added) values (6, 'Flynn', 'Skentelbury', 'fskentelbury5@netvibes.com', 'Male', 'PO Box 52536', 'Illinois', 'South Korea', 'Split-Type', '2023-11-10 01:04:57');
+insert into search_users_data (id, first_name, last_name, email, gender, address, state, nationality, ac_specialty, date_added) values (7, 'Shari', 'Learmont', 'slearmont6@rambler.ru', 'Female', 'Room 905', 'Wisconsin', 'Japan', 'Split-Type', '2024-05-27 17:18:48');
+insert into search_users_data (id, first_name, last_name, email, gender, address, state, nationality, ac_specialty, date_added) values (8, 'Constantine', 'Trott', 'ctrott7@youtube.com', 'Female', 'Suite 29', 'North Carolina', 'United States', 'Central Air-Type', '2024-04-19 00:09:37');
+insert into search_users_data (id, first_name, last_name, email, gender, address, state, nationality, ac_specialty, date_added) values (9, 'Lolly', 'Attiwill', 'lattiwill8@flavors.me', 'Female', 'Room 1564', 'Pennsylvania', 'Indonesia', 'Central Air-Type', '2024-07-09 05:44:19');
+insert into search_users_data (id, first_name, last_name, email, gender, address, state, nationality, ac_specialty, date_added) values (10, 'Errol', 'Deniset', 'edeniset9@go.com', 'Male', 'Apt 1759', 'Georgia', 'Iran', 'Window-Type', '2024-06-08 18:30:43');
+insert into search_users_data (id, first_name, last_name, email, gender, address, state, nationality, ac_specialty, date_added) values (11, 'Marie', 'Shovelin', 'mshovelina@businesswire.com', 'Female', 'Suite 60', 'North Carolina', 'China', 'Portable-Type', '2024-02-28 20:47:53');
+insert into search_users_data (id, first_name, last_name, email, gender, address, state, nationality, ac_specialty, date_added) values (12, 'Thoma', 'Scoggans', 'tscoggansb@yandex.ru', 'Male', 'PO Box 18722', 'Cebu', 'Brazil', 'Window-Type', '2024-04-02 03:40:54');
+insert into search_users_data (id, first_name, last_name, email, gender, address, state, nationality, ac_specialty, date_added) values (13, 'Ralf', 'Dunhill', 'rdunhillc@bbc.co.uk', 'Male', 'Suite 79', 'Davao', 'China', 'Portable-Type', '2023-11-12 06:41:21');
+insert into search_users_data (id, first_name, last_name, email, gender, address, state, nationality, ac_specialty, date_added) values (14, 'Fiona', 'Fleis', 'ffleisd@friendfeed.com', 'Female', 'PO Box 4257', 'Illinois', 'China', 'Window-Type', '2024-01-23 06:09:33');
+insert into search_users_data (id, first_name, last_name, email, gender, address, state, nationality, ac_specialty, date_added) values (15, 'Consuelo', 'Bartolomivis', 'cbartolomivise@nytimes.com', 'Female', 'Room 876', 'Texas', 'China', 'Split-Type', '2023-12-03 05:05:08');
+insert into search_users_data (id, first_name, last_name, email, gender, address, state, nationality, ac_specialty, date_added) values (16, 'Codie', 'Draayer', 'cdraayerf@flavors.me', 'Male', 'Suite 86', 'Quezon', 'Poland', 'Split-Type', '2024-05-06 14:50:28');
+insert into search_users_data (id, first_name, last_name, email, gender, address, state, nationality, ac_specialty, date_added) values (17, 'Annecorinne', 'Boake', 'aboakeg@aboutads.info', 'Female', 'Apt 237', 'Quezon', 'China', 'Window-Type', '2024-09-03 04:37:17');
+insert into search_users_data (id, first_name, last_name, email, gender, address, state, nationality, ac_specialty, date_added) values (18, 'Lewie', 'Skate', 'lskateh@shinystat.com', 'Male', 'Apt 938', 'Davao', 'Indonesia', 'Split-Type', '2023-11-30 18:03:54');
+insert into search_users_data (id, first_name, last_name, email, gender, address, state, nationality, ac_specialty, date_added) values (19, 'Nikolia', 'Geroldo', 'ngeroldoi@google.nl', 'Female', 'Apt 1953', 'New York', 'France', 'Split-Type', '2023-12-07 09:08:16');
+insert into search_users_data (id, first_name, last_name, email, gender, address, state, nationality, ac_specialty, date_added) values (20, 'Tricia', 'Roadnight', 'troadnightj@comsenz.com', 'Female', 'Room 603', 'Davao', 'China', 'Window-Type', '2024-06-28 19:13:09');
